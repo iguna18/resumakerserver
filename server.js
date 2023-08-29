9560d9581713bea3bd2df852806b2a1157087ccd
iguna18 <iguna18@freeuni.edu.ge>
2023-08-29 23:50:47 +0400

    nuffin

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const { spawn } = require("child_process");
-
+const PDFDocument = require("pdfkit");
 const app = express();
 const port = 3002;
 app.use(cors())
@@ -12,17 +12,40 @@ app.use(bodyParser.json());
 // Serve your static files if needed
 app.use(express.static("public"));
 
+
+app.post("/getpdf", (req, res) => {
+  const body = req.body;
+  console.log(body)
+/*
+  const doc = new PDFDocument();
+  doc.pipe(res);
+
+  doc.text(`Name: ${name}`);
+  doc.text(`Age: ${age}`);
+  doc.text(`Education: ${education}`);
+  if (experience !== null) {
+    doc.text(`Experience: ${experience}`);
+  }
+  doc.text(`Phone Number: ${phone_number}`);
+
+  doc.end();*/
+});
+
 app.post("/generate-pdf", (req, res) => {
   console.log('shesvla')
-  console.log(req.body)
+//  console.log(req.body)
   const { htmlContent } = req.body;
   if(!htmlContent) {
     console.log('ara')
   return
   }
-  console.log('ki')
+  console.log('ki', Object.keys(htmlContent))
   // Use wkhtmltopdf command to generate PDF
   const wkhtmltopdf = spawn("wkhtmltopdf", ["-", "-"]);
+  wkhtmltopdf.on("error", (error) => {
+    console.error("Error during PDF generation:", error);
+    res.status(500).send("Error during PDF generation");
+  });
   wkhtmltopdf.stdin.write(htmlContent);
   wkhtmltopdf.stdin.end();
 
@@ -30,7 +53,6 @@ app.post("/generate-pdf", (req, res) => {
   res.setHeader("Content-Type", "application/pdf");
 
   wkhtmltopdf.stdout.pipe(res);
-  res.end()
   return
 });
 
